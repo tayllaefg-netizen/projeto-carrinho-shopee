129b1db983e2e1aca9aaaca1c5e8a689def21485
tayllaefg-netizen <tayllaefg@gmail.com>
2026-05-14 19:40:32 -0300

    Revise cart items and update welcome message
    Updated console log message and replaced items in the cart with new feminine items. Added multiple items to the cart and removed redundant delete item operations.

--- src/index.js
+++ src/index.js
@@ -4,22 +4,31 @@ import createItem from "./services/item.js";
 const myCart = [];
 const myWhishList = [];
 
-console.log("Welcome to the your Shopee Cart!");
+console.log("Welcome to the Shopee Cart da Taylla!");
 
-//criando dois itens
-const item1 = await createItem("hotwheels ferrari", 20.99, 1);
-const item2 = await createItem("hotwheels lamborghini", 39.99, 3);
+// criando 10 itens femininos
+const item1 = await createItem("Vestido floral", 89.9, 1);
+const item2 = await createItem("Bolsa feminina", 119.9, 1);
+const item3 = await createItem("Sandalia rasteira", 59.9, 1);
+const item4 = await createItem("Perfume feminino", 139.9, 1);
+const item5 = await createItem("Kit maquiagem", 79.9, 1);
+const item6 = await createItem("Brinco dourado", 29.9, 1);
+const item7 = await createItem("Colar delicado", 34.9, 1);
+const item8 = await createItem("Blusa feminina", 49.9, 1);
+const item9 = await createItem("Saia jeans", 69.9, 1);
+const item10 = await createItem("Necessaire rosa", 39.9, 1);
 
-// adicionei dois itens ao carrinho
+// adicionei os itens ao carrinho
 await cartService.addItem(myCart, item1);
 await cartService.addItem(myCart, item2);
-
-await cartService.removeItem(myCart, item2);
-await cartService.removeItem(myCart, item2);
-await cartService.removeItem(myCart, item2);
+await cartService.addItem(myCart, item3);
+await cartService.addItem(myCart, item4);
+await cartService.addItem(myCart, item5);
+await cartService.addItem(myCart, item6);
+await cartService.addItem(myCart, item7);
+await cartService.addItem(myCart, item8);
+await cartService.addItem(myCart, item9);
+await cartService.addItem(myCart, item10);
 
 await cartService.displaycart(myCart);
-// deletei dois itens do carrinho
-// await cartService.deleteItem(myCart, item2.name);
-// await cartService.deleteItem(myCart, item1.name);
 await cartService.calculateTotal(myCart);
